9106a08b2b296ee1dc0a80a0a95d570f828add8e
superdupervenom <brysonhilton@gmail.com>
2018-12-19 05:03:45 -0800

    add footer to homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright">&copy; Test Lift <a href="https://html5up.net">Privacy Policy</a> <a href="https://www.gatsbyjs.org/">Terms & Conditions</a></p>
     </footer>
 )
 
