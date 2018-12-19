ccd2d9f4e946e5696996b56d82d7f32da128d606
superdupervenom <brysonhilton@gmail.com>
2018-12-18 21:31:29 -0800

    marketing language for homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,17 +8,18 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>We are partners in growing your company</h1>
+                <p>
+                    We are an exclusive network of the top freelance software developers, designers, marketers, and project managers 
+                </p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Software Development</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>UI/UX Design</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Marketing</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Project Management</a></li>
             </ul>
         </nav>
     </header>
